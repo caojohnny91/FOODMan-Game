c249175baee15c12bce93c6571472b80d3277988
jcao91 <jcao91@yahoo.com>
2024-05-20 14:10:05 -0500

    Changed up styling and added button els to the guessed letter

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const word = "ORANGE";
+const word = "ORANGE"; // do i need this?
 
+const winningWord = ['O', 'R', 'A', 'N', 'G', 'E'];
 
 /*-------------------------------- Variables --------------------------------*/
 
-let guessedLetters = [];
+let guessedLetters = []; // need to disable button when used
 
-let mistakes =0;
+let targetWord = ['', '', '', '', '', '']
 
+let mistakes = 0;
 
+const maxMistakes = 4;
+
+let winner = false;
 
 
 
@@ -18,16 +23,46 @@ let mistakes =0;
 
 /*------------------------ Cached Element References ------------------------*/
 
+const buttons = document.querySelectorAll('.guessed');
+// console.log(buttons);
 
 
 
+const messageEl = document.querySelector('#message');
+// console.log(messageEl);
+
+
+const targetEls = document.querySelectorAll('.target')
+// console.log(targetEl);
 
 
 
-
+const resetBtnEl = document.querySelector('.reset');
+// console.log(resetBtnEl);
 
 /*-------------------------------- Functions --------------------------------*/
 
+// const init = () => {
+//     // guessedLetters = []; not sure about this
+//     targetWord = ['', '', '', '', '', '']
+//     mistakes = 0;
+//     maxMistakes = 4;
+//     winner = false;
+//     render ();
+// }
+
+const render = () => {
+}
+
+const updateMessage = () => {
+}
+
+const updateTargetWord = () => {
+    targetWord.forEach((element, idx) => {
+        const targetEl = targetEls[idx];
+        targetEl.textContent = element; // not sure about this
+    })
+}
 
 
 
@@ -35,7 +70,8 @@ let mistakes =0;
 
 
 
-
-
-
+// init();
 /*----------------------------- Event Listeners -----------------------------*/
+
+
+
