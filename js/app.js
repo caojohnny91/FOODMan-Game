e3723cd7eb2eb31691e7a3718b6b53be85055bda
jcao91 <jcao91@yahoo.com>
2024-05-21 14:20:41 -0500

    hangleGuessedLetter

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const winningWord = ['O', 'R', 'A', 'N', 'G', 'E'];
 
 /*-------------------------------- Variables --------------------------------*/
 
-let guessedLetters = []; // need to disable button when used
+let guessedLetters = [];
 
 let targetWord = ['', '', '', '', '', '']
 
@@ -14,7 +14,7 @@ const maxMistakes = 4;
 
 let winner = false;
 
-let msg = '';
+let msg;
 
 
 
@@ -46,64 +46,81 @@ const init = () => {
     guessedLetters = [];
     targetWord = ['', '', '', '', '', '']
     mistakes = 0;
-    maxMistakes = 4;
     winner = false;
     guessedButtonsEl.forEach(button => {
         button.disabled = false;
     });
+    updateTargetWord();
+    updateMessage();
     render();
 };
 
-const render = () => {
-    targetWord.forEach((letter, idx) => {
-        targetEls[idx].textContent = letter;
-    });
-    resultDisplayEl.textContent = msg;
-    updateTargetWord();
-};
 
-const updateMessage = () => {
-    if (winner) {
-        msg = 'Congratulations! You Won!'
-    } else if (mistake >= maxMistakes) {
-        msg = 'Sorry! You lost! The word was' + winningWord.join('');
-    } else {
-        msg = `You have ${maxMistakes - mistakes} mistakes left!`
-    }
-};
 
-const updateTargetWord = () => {
-    targetWord.forEach((element, idx) => {
-        const targetEl = targetEls[idx];
-        targetEl.textContent = element;
-    })
-}
 
 
 const handleGuessedLetters = (event) => {
-    if (event.target.tagName === 'BUTTON') {
-        const guessedButton = event.target;
-        const guessedLetter = guessedButton.textContent;
+    winningWord.forEach((letter) => {
+        if (letter === event.target.id) {
+            targetWord[idx] = guessedButton;
+            updateTargetWord();
+            // correct guessed
+        } else {
+            mistakes++;
+            //reveal part of Foodman
+        }
+
+        // guessedButton.disabled = true;
+    })
+
+
+
+
+
+
+    // if (event.target.tagName === 'BUTTON' || event.target.tagName === 'DIV') {
+    //     const guessedButton = event.target;
+    //     const guessedLetter = guessedButton.textContent;
+    // }
+    
+    
+    
+};
+
+// const disableGuessedButtons = (handleGuessedLetters) => {
+    //     const guessedButtonsEl = document.querySelectorAll('.guessed-letters button');
+    //     guessedButtonsEl.forEach(guessedButton => guessedButton.disabled = true);
+    // };
+    // console.dir(disableGuessedButtons);
+    
+    const updateMessage = () => {
+        if (winner) {
+            msg = 'Congratulations! You Won!'
+        } else if (mistake >= maxMistakes) {
+            msg = 'Sorry! You lost! The word was' + winningWord.join('');
+        } else {
+            msg = `You have ${maxMistakes - mistakes} mistakes left!`
+        }
+    };
+    
+    const updateTargetWord = () => {
+        targetWord.forEach((element, idx) => {
+            const targetEl = targetEls[idx];
+            targetEl.textContent = element;
+        })
     }
-
-
-
-
-};
-
-const disableGuessedButtons = (handleGuessedLetters) => {
-    const guessedButtonsEl = document.querySelectorAll('.guessed');
-    guessedButtonsEl.forEach(guessedButton => guessedButton.disabled = true);
-};
-// console.dir(disableGuessedButtons);
-
-
-
-// init();
-
-/*----------------------------- Event Listeners -----------------------------*/
-guessedButtonsEl.forEach(button => {
-    document.addEventListener('click', handleGuessedLetters);
-});
-
-resetBtnEl.addEventListener('click', init);
+    
+    
+    const render = () => {
+        resultDisplayEl.textContent = `${msg}`;
+    };
+    
+    
+    // init();
+    
+    /*----------------------------- Event Listeners -----------------------------*/
+    guessedButtonsEl.forEach(button => {
+        button.addEventListener('click', handleGuessedLetters);
+    });
+    
+    resetBtnEl.addEventListener('click', init);
